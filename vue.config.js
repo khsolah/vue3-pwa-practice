6214422c3a19948540c940d9ce794c99ad2fa337
James Wu <james_wu@appdet.com>
2021-04-06 17:24:33 +0800

    + manifest config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,39 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue3-pwa-practice'
-    : ''
+  // publicPath: process.env.NODE_ENV === 'production' ? '/vue3-pwa-practice' : '',
+  pwa: {
+    name: 'vue3-pwa-practice (config in vue.config.js)',
+    theme_color: '#000',
+    msTile_color: '#aaa',
+    appleMobileWebAppCache: 'yes',
+    manifestOptions: {
+      theme_color: "#000",
+      background_color: "#bbb",
+      display: 'fullscreen',
+      orientation: 'landscape-secondary',
+      icons: [
+        {
+          src: './img/icons/android-chrome-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/android-chrome-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: './img/icons/android-chrome-maskable-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+          purpose: 'maskable'
+        },
+        {
+          src: './img/icons/android-chrome-maskable-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ]
+    }
+  }
 }
