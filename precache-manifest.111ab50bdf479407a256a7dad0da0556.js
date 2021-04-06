5eba2c924437acbb486a7ce0be6dcd67a978b67b
James Wu <james_wu@appdet.com>
2021-04-06 18:09:21 +0800

    deploy

---
+++ precache-manifest.111ab50bdf479407a256a7dad0da0556.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a912889e1aec7e3dd22",
+    "url": "/vue3-pwa-practice/css/app.b3364b4a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue3-pwa-practice/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5f9a8bb9a0aa0a1e04e442600014feb1",
+    "url": "/vue3-pwa-practice/index.html"
+  },
+  {
+    "revision": "d8610e7df79d80c0c835",
+    "url": "/vue3-pwa-practice/js/about.90b284f2.js"
+  },
+  {
+    "revision": "3a912889e1aec7e3dd22",
+    "url": "/vue3-pwa-practice/js/app.5dc5f60f.js"
+  },
+  {
+    "revision": "e54d6cc1b8caf57932ee",
+    "url": "/vue3-pwa-practice/js/chunk-vendors.d0e8d2f4.js"
+  },
+  {
+    "revision": "0fe59858143f2cc032196452b7c76b78",
+    "url": "/vue3-pwa-practice/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-pwa-practice/robots.txt"
+  }
+]);
